2e9f963a43b0198dddba97f68512cc74bc0821e1
Kubilay Erdogan <kublaios@gmail.com>
2018-03-01 16:16:01 +0300

    ngClass

--- src/app/server/server.component.ts
+++ src/app/server/server.component.ts
@@ -2,7 +2,15 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-server',
-  templateUrl: './server.component.html'
+  templateUrl: './server.component.html',
+  styles: [`
+    .online {
+      color: white;
+    }
+    . offline {
+      
+    }
+  `],
 })
 export class ServerComponent {
   serverId = 10;

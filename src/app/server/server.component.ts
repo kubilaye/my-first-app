a3e1b6bad8bf38bb2e46a2f97cdb6765f3482530
Kubilay Erdogan <kublaios@gmail.com>
2018-03-01 16:13:11 +0300

    ngStyle usage with property binding

--- src/app/server/server.component.ts
+++ src/app/server/server.component.ts
@@ -9,7 +9,15 @@ export class ServerComponent {
   serverStatus = 'offline';
   // serverStatus: string = 'offline';
 
+  constructor() {
+    this.serverStatus = Math.random() > 0.5 ? 'online' : 'offline';
+  }
+
   getServerStatus() {
     return this.serverStatus;
+  }
+
+  getColor() {
+    return this.serverStatus === 'online' ? 'green' : 'yellow';
   }
 }

49f5c7d54604423e0b716bbd69f4eb9e861ce0cb
Kubilay Erdogan <kublaios@gmail.com>
2018-03-01 10:04:17 +0300

    passing and using data with event binding

--- src/app/servers/servers.component.ts
+++ src/app/servers/servers.component.ts
@@ -9,6 +9,7 @@ export class ServersComponent implements OnInit {
 
   allowNewServer = false;
   serverCreationStatus = 'No server was created.';
+  serverName = '';
 
   constructor() {
     setTimeout(() => {
@@ -20,7 +21,11 @@ export class ServersComponent implements OnInit {
   }
 
   onCreateServer() {
-    this.serverCreationStatus = 'Server was created.'
+    this.serverCreationStatus = 'Server was created.';
+  }
+
+  onUpdateServerName(event: any) {
+    this.serverName = (<HTMLInputElement>event.target).value;
   }
 
 }
